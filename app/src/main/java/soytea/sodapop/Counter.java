bf0f7ce0c9e48fa30f8a9f19ba330e3824394278
Shaw <shaw.tan@uwaterloo.ca>
2015-01-31 15:32:15 -0500

    runnable

--- app/src/main/java/soytea/sodapop/Counter.java
+++ app/src/main/java/soytea/sodapop/Counter.java
@@ -1,8 +1,5 @@
 package soytea.sodapop;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 /**
  * Created by shaw on 15-01-31.
  */
@@ -12,15 +9,12 @@ public class Counter {
     private int multiplier;
     private int passive;
 
-    Timer timer;
-
     public Counter() {
         count = 0;
         multiplier = 1;
         passive = 0;
+        new Passive(this, 1000).run();
 
-        timer = new Timer();
-        timer.schedule(new TimerHandler(this), 1000);
     }
 
     public void add1(){
@@ -39,24 +33,47 @@ public class Counter {
         this.passive += p;
     }
 
+    public void passiveTick(){
+        this.count += passive;
+    }
+
     public int getCount(){
         return count;
     }
 
-    private class TimerHandler extends TimerTask{
+    private class Passive implements Runnable {
 
         Counter counter;
+        long startTime, sleepTime;
+        long time;
+        Boolean running;
 
-        public TimerHandler(Counter c) {
+
+        public Passive(Counter c, long time){
 
             this.counter = c;
+            this.time = time;
+            this.running = true;
 
         }
 
         @Override
         public void run() {
-            counter.count += counter.passive;
+
+            while (running) {
+                startTime = System.currentTimeMillis();
+                sleepTime = time-(System.currentTimeMillis() - startTime);
+                try {
+                    if (sleepTime > 0)
+                        Thread.sleep(sleepTime);
+                    else
+                        Thread.sleep(1000);
+                } catch (Exception e) {}
+                counter.passiveTick();
+            }
         }
+
+
     }
 
 }
